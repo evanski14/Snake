37bfb31ef7ad5279ba305fb58cb4bfda77385a84
evanski14 <killmasterjoy13@live.com>
2017-11-03 09:32:41 +0100

    last commit

--- models.py
+++ models.py
@@ -1,4 +1,16 @@
+DIR_UP = 1
+DIR_RIGHT = 2
+DIR_DOWN = 3
+DIR_DOWN = 4
+
+DIR_OFFSET = {DIR_UP:(0,1), DIR_RIGHT:(1,0), DIR_DOWN:(0,-1), DIR_LEFT:(-1,0)}
+
+
+
+
+
 class Snake:
+    BLOCK_SIZE = 16
 
     MOVE_WAIT = 0.2
 
@@ -9,15 +21,17 @@ class Snake:
 
         self.wait_time = 0
 
+        self.direction = DIR_RIGHT
+
     def update(self, delta):
         self.wait_time += delta
 
         if self.wait_time < Snake.MOVE_WAIT:
             return
 
-        if self.x > self.world.width:
-            self.x = 0
-        self.x += 16
+        self.x = 0
+        self.y = 0
+
         self.wait_time = 0
 
 
